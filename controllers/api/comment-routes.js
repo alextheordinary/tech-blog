f4403473a9d62855978ce872ebaa3e20014264b9
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-08 16:31:36 -0400

    wrote a get and post route for comment-routes. going to test in insomnia

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,34 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
+// Endpoint for /api/comments
+
+// Returns an array of all comments associated with a post id + creator name of comment
+router.get('/:id', async (req, res) => {
+    try {
+        const comments = await Comment.findByPk(req.params.id, {
+            include: [{
+                model: User, attributes: ['name']
+            }]
+        });
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// Creates a new comment
+router.post('/', async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            content: req.body.content,
+            user_id: req.body.user_id,
+            post_id: req.body.post_id
+        });
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
