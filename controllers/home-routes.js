fdf0f42e15ef615298557ddf7f679015bb84279d
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-09 19:06:58 -0400

    set up everything to display a single post with comments. still need to implement add comment

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,12 @@ const queryHelpers = require('../utils/query-helpers');
 
 router.get('/', async (req, res) => {
     const posts = await queryHelpers.getPosts();
-    res.render('homepage', {posts});
-  });
-  
-  module.exports = router;
+    res.render('homepage', { posts });
+});
+
+router.get('/posts/:id', async (req, res) => {
+    const post = await queryHelpers.getSinglePost(req.params.id);
+    res.render('post', { post });
+});
+
+module.exports = router;
--- utils/query-helpers.js
+++ utils/query-helpers.js
@@ -1,4 +1,5 @@
 // importing models
+const { post } = require('../controllers/home-routes');
 const {User, Post, Comment} = require('../models');
 
 module.exports = {
@@ -11,13 +12,30 @@ module.exports = {
         const posts = postData.map(post => post.get({plain: true}));
         return posts;
     },
-    getUserPosts: async (id) => {
+    getSinglePost: async (id) => {
         const postData = await Post.findByPk(id, {
             include: [{
                 model: User, attributes: ['name']
-            }]
+            },
+            {model: Comment, include: {model: User}}
+        ]
         });
-        const posts = postData.map(post => post.get({plain: true}));
+        const posts = postData.get({plain: true});
         return posts;
+    },
+    getPostComments: async (id) => {
+        const commentData = await Comment.findAll({
+            include: [{
+                model: User, attributes: ['name']
+            }]
+        },
+            {
+                where: {
+                    post_id: id
+                }
+            }
+        );
+        const comments = commentData.map(comment => comment.get({plain: true}));
+        return comments;
     },
 }
