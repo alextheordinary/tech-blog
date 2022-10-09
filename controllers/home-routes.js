c8637505c7482d914a27be71ff34faee219eec34
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-09 16:08:09 -0400

    displayed post titles on the main page with handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const {User, Post, Comment} = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('homepage');
+    const postData = await Post.findAll({
+        include: [{
+            model: User, attributes: ['name']
+        }]
+    });
+    const posts = postData.map(post => post.get({plain: true}));
+    res.render('homepage', {posts});
   });
   
   module.exports = router;
