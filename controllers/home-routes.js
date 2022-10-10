516292223601960dfb012ef4cd7444b9d5a98973
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-10 12:01:59 -0400

    added basic login layout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,7 +32,7 @@ router.get('/dashboard', async (req, res) => {
 
 router.get('/login', async (req, res) => {
     try {
-
+        res.render('login')
     } catch (err) {
         res.status(400).json(err);
     }
