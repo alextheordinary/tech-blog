724ee980f06c148bca56e64479a4829834f0715e
Alex Bellamy <alexjbellamy@gmail.com>
2022-10-11 10:11:53 -0400

    changed it so user is logged in on signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,21 +1,31 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector("#username-login").value.trim();
-    const password = document.querySelector("#password-login").value.trim();
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-    if (username && password) {
-        const response = await fetch('api/users/', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            document.location.replace('/');
-          } else {
-            alert('Failed to signup');
-          }
+  if (username && password) {
+    const response = await fetch('api/users/', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      // Login and go to home page
+      const response = await fetch('api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    } else {
+      alert('Failed to signup');
     }
+  }
 };
 
 document
